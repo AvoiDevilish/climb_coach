cb93d9bb7445a1d971f5023be577d311f9bbfb1e
DevilishCorePlatform <irclimb.ir@outlook.com>
2026-07-24 15:50:11 +0330

    Define database schema tables

---
+++ lib/core/database/tables.dart
@@ -0,0 +1,50 @@
+class Tables {
+  // ----------------------------
+  // Database
+  // ----------------------------
+
+  static const databaseName = 'climb_coach.db';
+  static const databaseVersion = 1;
+
+  // ----------------------------
+  // Athletes
+  // ----------------------------
+
+  static const athletes = 'athletes';
+
+  // ----------------------------
+  // Dynamic Fields
+  // ----------------------------
+
+  static const customFields = 'custom_fields';
+
+  // ----------------------------
+  // Athlete Values
+  // ----------------------------
+
+  static const athleteValues = 'athlete_values';
+
+  // ----------------------------
+  // Assessments
+  // ----------------------------
+
+  static const assessments = 'assessments';
+
+  // ----------------------------
+  // Assessment Results
+  // ----------------------------
+
+  static const assessmentResults = 'assessment_results';
+
+  // ----------------------------
+  // Movements
+  // ----------------------------
+
+  static const movements = 'movements';
+
+  // ----------------------------
+  // Settings
+  // ----------------------------
+
+  static const settings = 'settings';
+}
